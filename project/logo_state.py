604061c8aba74a4dab93d0899aa4a310ba683dd6
nemonom <nemonom230@gmail.com>
2017-10-25 12:28:23 +0900

    logo animation

--- project/logo_state.py
+++ project/logo_state.py
@@ -7,16 +7,22 @@ from pico2d import *
 name = "logo_state"
 image = None
 logo_time = 0.0
-
+logo_rotate = 0.0
 
 def enter():
     global logo
+    global logo_back
+
     open_canvas(1000, 700)
     logo = load_image('logo.png')
+    logo_back = load_image('logo_back.png')
 
 def exit():
-    global image
-    del(image)
+    global logo
+    global logo_back
+
+    del(logo)
+    del(logo_back)
     close_canvas()
 
 def update():
@@ -28,13 +34,20 @@ def update():
         game_framework.push_state((main_state))
     delay(0.01)
     logo_time += 0.01
-    
+
 def draw():
     global logo
-    clear_canvas()
-    logo.draw(500, 350)
-    update_canvas()
+    global logo_back
+    global logo_rotate
 
+    clear_canvas()
+   # logo.draw(500, 350)
+    logo_back.draw(500, 350)
+    logo.rotate_draw(logo_rotate, 500, 350, 1000 - logo_rotate * 50, 700 - logo_rotate * 50)
+
+    update_canvas()
+    if(logo_time > 1.3):
+        logo_rotate += 0.2
 def handle_events():
     events = get_events()
     pass
