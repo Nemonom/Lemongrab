fa42bd59c279827e236b3cd2684428df5bd2d454
nemonom <nemonom230@gmail.com>
2017-10-27 17:13:46 +0900

    exit button

--- project/button_class.py
+++ project/button_class.py
@@ -3,25 +3,33 @@ from pico2d import *
 
 class button:
     button_img = None
-    m_x, m_y = 0, 0
-    s_x, s_y = 0, 0
+    m_x = 0
+    m_y = 0
+    s_x = 0
+    s_y = 0
 
     mouse_on = False
 
     def __init__(self, name, x_local, y_local, x_size, y_size):
-        if self.button_img == None:
-            self.button_img = load_image(name)
-        m_x = x_local, m_y = y_local
-        s_x = x_size, s_y = y_size
+        if button.button_img == None:
+            button.button_img = load_image(name)
+        self.m_x = x_local
+        self.m_y = y_local
+        self.s_x = x_size
+        self.s_y = y_size
 
     def draw(self):
-        self.button_img.draw(self, self.m_x, self.m_y
-                             , self.m_x - self.s_x, self.m_y - self.s_y)
+        self.button_img.draw(self.m_x, self.m_y
+                             , self.s_x * 2, self.s_y * 2)
 
-    def click(self, mouse_x, mouse_y):
-        events = get_events()
-        for event in events:
-            if event.type == SDL_MOUSEBUTTONDOWN and event.button == SDL_BUTTON_LEFT: #LBUTTON CLICK
-                if(self.m_x - self.s_x <= event.x and event.x <= self.m_x + self.s_x  #COLLISION
-                   and self.m_y - self.s_y <= event.y and event.y <= self.m_y + self.s_y):
-                    mouse_on = True
+        if self.mouse_on:
+            self.button_img.draw(500, 500, 500, 500)
+
+    def mousemove_on(self, mouse_x, mouse_y):
+        if(self.m_x - self.s_x <= mouse_x and mouse_x <= self.m_x + self.s_x and self.m_y - self.s_y <= mouse_y and mouse_y <= self.m_y + self.s_y):
+            self.mouse_on = True
+        else:
+            self.mouse_on = False
+
+    def get_mouse_on(self):
+        return self.mouse_on
--- project/main_state.py
+++ project/main_state.py
@@ -1,32 +1,48 @@
 import global_parameters
 import game_framework
 import mouse_pointer
+import button_class
+
 from pico2d import *
 
 
 name = "main_state"
 image = None
 main_pointer = None
+exit_but = None
 width = global_parameters.width
 height = global_parameters.height
 
 def enter():
     global background
     global main_pointer
-    main_pointer = mouse_pointer.pointer()
-    main_pointer.__init__()
+    global exit_but
+
     background = load_image('main.png')
+    main_pointer = mouse_pointer.pointer()
+    exit_but = button_class.button('exit_button.png', 900, 50, 80, 40)
+
 
 def exit():
     global background
     global main_pointer
+    global exit_but
+
     del(background)
     del(main_pointer)
+    del(exit_but)
+
 
 def handle_events():
     events = get_events()
     for event in events:
-        if event.type == SDL_QUIT:
+        if event.type == SDL_MOUSEMOTION:
+            exit_but.mousemove_on(event.x, 700 - event.y)
+
+        if exit_but.get_mouse_on() == True and (event.type, event.button) == (SDL_MOUSEBUTTONDOWN, SDL_BUTTON_LEFT):
+            game_framework.quit()
+
+        elif event.type == SDL_QUIT:
             game_framework.quit()
         else:
             if(event.type, event.key) == (SDL_KEYDOWN, SDLK_ESCAPE):
@@ -38,6 +54,7 @@ def draw():
     hide_cursor()
     background.draw(width/2, height/2)
     main_pointer.draw(0)
+    exit_but.draw()
     update_canvas()
 
 
@@ -47,6 +64,7 @@ def draw():
 
 def update():
     main_pointer.update()
+
     pass
 
 
